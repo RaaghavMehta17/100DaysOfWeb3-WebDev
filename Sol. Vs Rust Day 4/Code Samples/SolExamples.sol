430f71fe67ce110c30fcb69a3331b0db0bc6cd8a
RaaghavMehta17 <75270837+RaaghavMehta17@users.noreply.github.com>
2023-05-21 23:56:59 +0530

    Update SolExamples.sol

--- Sol. Vs Rust Day 4/Code Samples/SolExamples.sol
+++ Sol. Vs Rust Day 4/Code Samples/SolExamples.sol
@@ -1,1 +1,53 @@
+// Access Control 
+pragma solidity ^0.8.0;
+
+contract AccessControlExample {
+    address public owner; // Public variable to store the address of the contract owner
+
+    constructor() {
+        owner = msg.sender; // Set the contract deployer's address as the initial owner
+    }
+
+    modifier onlyOwner() { // Modifier to restrict access to certain functions to only the contract owner
+        require(msg.sender == owner, "Only the contract owner can call this function.");
+        _; // Placeholder indicating where the modified function's code will be inserted
+    }
+
+    function restrictedFunction() public onlyOwner {
+        // Function code that can only be executed by the contract owner
+    }
+}
+
+
+// Validate contract addresses
+
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    function transferFunds(address payable recipient) public {
+        require(recipient != address(0), "Invalid recipient address.");
+
+        // Use transfer() to send funds
+        recipient.transfer(msg.value);
+    }
+}
+
+//Avoid reentrancy attacks
+
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    bool private locked;
+
+    modifier nonReentrant() {
+        require(!locked, "Reentrant call detected");
+        locked = true;
+        _;
+        locked = false;
+    }
+
+    function withdraw() external nonReentrant {
+        // Perform withdrawal logic
+    }
+}
 
